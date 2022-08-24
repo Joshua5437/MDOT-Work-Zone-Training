79e0807aa18317af4352e3552f2e1b7072b5f799
Joshua Cotton <jic77@msstate.edu>
2022-08-24 15:18:19 -0500

    Minor Changes

--- MDOT - Highway Work Zone Training/Assets/Scripts/MoveRoadRoller.cs
+++ MDOT - Highway Work Zone Training/Assets/Scripts/MoveRoadRoller.cs
@@ -5,25 +5,51 @@ using System.Collections.Generic;
 
 public class MoveRoadRoller : MonoBehaviour
 {
+    private int turn = 0;
+    private GameObject LB_Wheel, RB_Wheel, Front_Wheel;
+
     public float Duration = 1.0f;
     public AudioSource BackingUpAudio;
 
     private void Start()
     {
-        StartCoroutine(DoFade(15, 0));
+        Front_Wheel = GameObject.Find("Front_Wheel");
+        LB_Wheel = GameObject.Find("Wheel_Left_Back.001");
+        RB_Wheel = GameObject.Find("Wheel_Right_Back.001");
+        StartCoroutine(ForwardAndReverseRoadRoller(15, 0));
     }
 
-    public IEnumerator DoFade(float start, float end)
+	private void Update () {
+
+        if (turn == 0) // Turns wheels forward. 
+        { 
+            Front_Wheel.transform.Rotate(0f, 1f, 0f, Space.Self);
+            LB_Wheel.transform.Rotate(0f, 1f, 0f, Space.Self);
+            RB_Wheel.transform.Rotate(0f, 1f, 0f, Space.Self); 
+        }
+
+        else           // Turns wheels backward.
+        {
+            Front_Wheel.transform.Rotate(0f, -1f, 0f, Space.Self); // Front wheel does not turn backwards.
+            LB_Wheel.transform.Rotate(0f, -1f, 0f, Space.Self); 
+            RB_Wheel.transform.Rotate(0f, -1f, 0f, Space.Self); 
+        }
+
+    }
+
+    public IEnumerator ForwardAndReverseRoadRoller(float start, float end)
     {
-        float counter = 0f;
+        float counter = 0f;             // Logic to move road roller forward. 
+         BackingUpAudio.Stop();
         while(counter < Duration)
         {
             counter += Time.deltaTime;
             transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(start, end, (counter / Duration)));
             yield return null;
         }
+        turn--;
 
-        counter = 0f;
+        counter = 0f;                   // Logic to reverse road roller.
         BackingUpAudio.Play();
         while(counter < Duration)
         {
@@ -31,6 +57,7 @@ public class MoveRoadRoller : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Lerp(end, start, (counter / Duration)));
             yield return null;
         }
-        StartCoroutine(DoFade(15, 0));
+        turn++;
+        StartCoroutine(ForwardAndReverseRoadRoller(15, 0)); // Recalls itself
     }
 }

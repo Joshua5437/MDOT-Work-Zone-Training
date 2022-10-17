cee7adf02066653d8f772dc191c07639e47949a9
Joshua Cotton <jic77@msstate.edu>
2022-10-17 11:46:27 -0500

    Finished Functions
    - Ready to begin adding other sections of the documents.

--- MDOT - Highway Work Zone Training/Assets/Scripts/CorrectPPE.cs
+++ MDOT - Highway Work Zone Training/Assets/Scripts/CorrectPPE.cs
@@ -5,31 +5,17 @@ using System.Collections.Generic;
 
 public class CorrectPPE : MonoBehaviour
 {
-    private bool failSafe = false;
-    public AudioSource BeginHazard;
-    public GameObject LBoot, RBoot, Hat, Class2Vest, T_Point, StartHazardButton;
+    private bool failSafe = true;
+    public GameObject LBoot, RBoot, Hat, Class2Vest, T_Point, StartHazardButton, CrossingBoard;
 
     private void Update()
     {
-        if(!LBoot.activeSelf && !Hat.activeSelf && !Class2Vest.activeSelf && !RBoot.activeSelf )
+        if(!LBoot.activeSelf && !Hat.activeSelf && !Class2Vest.activeSelf && !RBoot.activeSelf && failSafe)
         {
-            
+            failSafe = false;
             GameObject User = GameObject.Find("XR Origin");
             User.transform.position = new Vector3(T_Point.transform.position.x, T_Point.transform.position.y, T_Point.transform.position.z);
-            BeginHazard.Play();
-            StartCoroutine(WaitForInvoke());
+            CrossingBoard.active = true;
         }
-    }
-
-    private IEnumerator WaitForInvoke()
-    {
-        yield return new WaitForSeconds(3);
-        failSafe = true;
-        InvokeHazardButton();
-    }
-
-    private void InvokeHazardButton()
-    {
-        if(!BeginHazard.isPlaying && failSafe) { StartHazardButton.GetComponent<Button>().onClick.Invoke(); }
     }
 }
--- MDOT - Highway Work Zone Training/Assets/Scripts/MoveRoadRoller.cs
+++ MDOT - Highway Work Zone Training/Assets/Scripts/MoveRoadRoller.cs
@@ -9,7 +9,7 @@ public class MoveRoadRoller : MonoBehaviour
     private GameObject Wh1, Wh2, Wh3, Wh4, Wh5, Wh6;
     private GameObject LB_Wheel, RB_Wheel, Front_Wheel;
 
-    public GameObject Dumptruck;
+    public GameObject Dumptruck, RRCube, DPCube;
     public float Duration = 1.0f;
     public AudioSource BackingUpAudio;
 
@@ -71,6 +71,7 @@ public class MoveRoadRoller : MonoBehaviour
 
     public IEnumerator HazardScenario2(float start, float end)
     {
+        
         float counter = 0f;   
         counter = 0f;                   // Logic to reverse road roller.
         BackingUpAudio.Play();
@@ -82,6 +83,8 @@ public class MoveRoadRoller : MonoBehaviour
             Dumptruck.transform.position = new Vector3(Dumptruck.transform.position.x, Dumptruck.transform.position.y, Mathf.Lerp(35, 30, (counter / Duration)));
             yield return null;
         }
+        DPCube.GetComponent<TestObject>().enabled = true;
+        RRCube.GetComponent<TestObject>().enabled = true;
         turn = -1;
         // StartCoroutine(ForwardAndReverseRoadRoller(15, 0)); // Recalls itself
     }

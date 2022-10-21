9c33e1e85e2f1e6d1b40c9cd9d6e190915e8bf83
Joshua Cotton <jic77@msstate.edu>
2022-10-21 13:20:27 -0500

    Minor Changes

---
+++ MDOT - Highway Work Zone Training/Assets/Scripts/ReturnObject.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReturnObject : MonoBehaviour
+{
+    private float PositionX, PositionY, PositionZ;
+    private float RotationX, RotationY, RotationZ;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Collects the starting position of the RoadCone. 
+        PositionX = transform.localPosition.x;
+        PositionY = transform.localPosition.y;
+        PositionZ = transform.localPosition.z;
+
+        // Collects the starting rotation of the RoadCone
+        RotationX = transform.localRotation.x;
+        RotationY = transform.localRotation.y;
+        RotationZ = transform.localRotation.z;
+
+    }
+
+    public void ReturnObjectToTable()
+    {
+        transform.localPosition = new Vector3(PositionX, PositionY, PositionZ);
+        transform.localRotation = Quaternion.Euler(RotationX, RotationY, RotationZ);
+    }
+}
